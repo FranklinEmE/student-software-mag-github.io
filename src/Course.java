3f2be3773e11af109a0194bb97d809a98acccc8c
FRANKLIN  EMMANUEL .C <51802318+FranklinEmE@users.noreply.github.com>
2020-02-12 11:47:33 +0100

    commit

---
+++ src/Course.java
@@ -0,0 +1,167 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+
+public class Course {
+  
+    
+    
+   public void insertUpdateDeleteStudent(char operation, Integer id, String label, Integer hours){
+                                       
+         
+        Connection con = MyConnection.getConnection();
+        PreparedStatement ps;
+        
+        if(operation == 'i'){
+            try {
+                ps = con.prepareStatement("INSERT INTO course (label, hours_number)  VALUE (?,?)");
+                  
+
+                ps.setString(1, label);
+                ps.setInt(2, hours);
+                if(ps.executeUpdate() > 0){
+                    JOptionPane.showMessageDialog(null, "New Course Added");
+                }
+
+            } catch (SQLException ex) {
+                Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+      if(operation == 'u'){
+            try {
+                ps = con.prepareStatement("UPDATE course SET label = ?, hours_number =? WHERE id = ?");
+                ps.setString(1, label);
+                ps.setInt(2, hours);
+             
+                ps.setInt(3, id);
+
+
+                if(ps.executeUpdate() > 0){
+                    JOptionPane.showMessageDialog(null, "Course Data Updated");
+                }
+
+            } catch (SQLException ex) {
+                Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+  
+    
+      if(operation == 'd'){
+          
+           int  YesOrNo = JOptionPane.showConfirmDialog(null, "The Scores Will Be Also Deleted", "Delete Scores", JOptionPane.OK_CANCEL_OPTION, 0);
+          if(YesOrNo == JOptionPane.OK_CANCEL_OPTION){
+           
+                try {
+                ps = con.prepareStatement("DELETE FROM Course WHERE id = ?");
+                ps.setInt(1, id);
+
+
+                if(ps.executeUpdate() > 0){
+                    JOptionPane.showMessageDialog(null, "course Deleted");
+                }
+
+            } catch (SQLException ex) {
+                Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
+            }
+          }
+          
+        }
+   } 
+   
+   public boolean isCourseExist(String courseName){
+       
+       boolean isExist = false;
+       Connection con = MyConnection.getConnection();
+       PreparedStatement ps;
+       try {
+        ps = con.prepareStatement("SELECT * FROM course WHERE label = ?");
+        ps.setString(1, courseName);
+        ResultSet rs = ps.executeQuery();
+                
+            if(rs.next()){
+            isExist = true;
+
+                  }
+            } catch (SQLException ex) {
+            Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   
+       
+            return  isExist;
+   }
+   
+   
+   
+            public void fillCourseJtable(JTable table) {
+            Connection con = MyConnection.getConnection();
+            PreparedStatement ps;
+            try {
+            ps = con.prepareStatement("SELECT * FROM course");
+                  ResultSet rs = ps.executeQuery();
+                  DefaultTableModel model = (DefaultTableModel)table.getModel();
+                  
+                  Object[] row;
+               //  LIKE? ("SELECT * FROM studentinfor WHERE CONCAT('first_name', 'last_name', 'phone', 'address')LIKE ?");
+                  while(rs.next()){
+                      row = new Object[3];
+                      row[0] = rs.getInt(1);
+                      row[1] = rs.getString(2);
+                      row[2] = rs.getInt(3);
+                   
+                      
+                      model.addRow(row);
+
+                  }
+            } catch (SQLException ex) {
+            Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+    
+            public int getCourseId(String courseLabel){
+           int courseId = 0;
+        
+            Connection con = MyConnection.getConnection();
+            PreparedStatement ps;
+            try {
+            ps = con.prepareStatement("SELECT * FROM course WHERE label = ?");
+             ps.setString(1, courseLabel);
+             ResultSet rs = ps.executeQuery();
+                
+               //  LIKE? ("SELECT * FROM studentinfor WHERE CONCAT('first_name', 'last_name', 'phone', 'address')LIKE ?");
+               if(rs.next()){
+               courseId = rs.getInt("Id");
+                  }
+               } catch (SQLException ex) {
+               Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
+      }
+        
+               return courseId;
+    }
+
+       public void fillCourseCombo(JComboBox combo) {
+       Connection con = MyConnection.getConnection();
+       PreparedStatement ps;
+      try {
+          ps = con.prepareStatement("SELECT * FROM course");
+          ResultSet rs = ps.executeQuery();
+                
+               //  LIKE? ("SELECT * FROM studentinfor WHERE CONCAT('first_name', 'last_name', 'phone', 'address')LIKE ?");
+                  while(rs.next()){
+                
+                combo.addItem(rs.getString(2));
+
+                  }
+      } catch (SQLException ex) {
+          Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+}
